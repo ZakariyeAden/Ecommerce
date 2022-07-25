e33d30189d115eeb661a185e115406b1fccf6c8e
ZakariyeAden <zakariye5417@gmail>
2022-07-25 15:16:15 -0500

    Updating styling

---
+++ src/Components/Style/style-components/index.js
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+// Home Section
+export const MainHeading = styled.h1`
+  font-size: 3.5rem;
+  letter-spacing: 0.2em;
+  color: #fff;
+  font-style: bold;
+  font-weight: 700;
+`;
+export const Container = styled.section`
+  max-width: 1440px;
+`;
+export const Button = styled.button`
+  font-size:1rem;
+  border:none;
+  background-color:#3a3a3a;
+  padding: 1rem 2rem;
+  color:#fff
+`;
+export const SectionContainer = styled.div`
+  padding:7rem 4rem;
+`;
+export const Image2 = styled.img`
+  width:98%
+`;
+
+// Reuseable
+export const SecondaryHeading = styled.h3`
+  font-size:2rem;
+  letter-spacing:0.25rem;
+  color:#3a3a3a;
+  text-align:center;
+`;
+export const Image = styled.img`
+  width:100%
+`;
+export const Description = styled.p`
+  text-align:center;
+  font-size:1rem;
+  line-height:1.5;
+  font-weight:100;
+`;
