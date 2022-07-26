e3e7763bd09eb6ba064a0ef7937e2087b73ab91c
ZakariyeAden <zakariye5417@gmail>
2022-07-26 15:57:40 -0500

    products

---
+++ src/Components/Shop/index.js
@@ -0,0 +1,65 @@
+import BrownThobe from '../Assets/Thobes/BrownThobe.jpg'
+import GoldThobe from '../Assets/Thobes/Gold.jpg'
+import WhiteThobe from '../Assets/Thobes/White.jpg'
+import BlackCap from '../Assets/Caps/BlackCap.png'
+import GrayCap from '../Assets/Caps/CapGray.png'
+import AbayaCream from '../Assets/Abaya/AbayaCream.png'
+import GreenAbaya from '../Assets/Abaya/GreenAbaya.png'
+
+ const products = [
+  {
+    id: "p1",
+    price: 12.99,
+    title: "Gold Thobe",
+    src: GoldThobe,
+    button:"Add to Cart",
+  },
+  {
+    id: "p2",
+    price: 12.99,
+    title: "White Thobe",
+    src: WhiteThobe,
+    button:"Add to Cart",
+  },
+  {
+    id: "p3",
+    price: 12.99,
+    title: "Brown Thobe",
+    src: BrownThobe,
+    button:"Add to Cart",
+  },
+  {
+    id: "p4",
+    price: 11.99,
+    title: "Abaya Cream",
+    src: AbayaCream,
+    width:"200",
+    button:"Add to Cart",
+  },
+  {
+    id: "p5",
+    price: 11.99,
+    title: "Green Abaya",
+    src: GreenAbaya,
+    width:"200",
+    button:"Add to Cart",
+  },
+  {
+    id: "p6",
+    price: 8.99,
+    title: "Black Cap",
+    src: BlackCap,
+    width:"200",
+    button:"Add to Cart",
+  },
+  {
+    id: "p7",
+    price: 8.99,
+    title: "Gray Cap",
+    src: GrayCap,
+    width:"200",
+    button:"Add to Cart",
+  },
+];
+
+export default products;
