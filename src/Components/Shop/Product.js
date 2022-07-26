9eb31d6e2c36b155f7992fd7c11906b169024e5d
ZakariyeAden <zakariye5417@gmail>
2022-07-26 12:57:05 -0500

    starting on products

---
+++ src/Components/Shop/Product.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { SectionContainer } from '../Style/style-components'
+import { Grid } from "@mui/material";
+function Product() {
+  return (
+    <div>
+      <SectionContainer>
+        <Grid container spacing={4}>
+          <div>
+          
+          </div>
+          <div>
+            
+            </div>
+            <div>
+            
+            </div>
+            <div>
+            
+            </div>
+        </Grid>
+      </SectionContainer>
+    </div>
+  )
+}
+
+export default Product
