0efdc239d4f7f06c279ccff4c693f8ac433e2df9
ZakariyeAden <zakariye5417@gmail>
2022-10-29 14:24:46 -0500

    _

--- src/Components/Shop/ProductItems.js
+++ src/Components/Shop/ProductItems.js
@@ -1,39 +1,40 @@
 import React from "react";
-import { SectionContainer,ProductHeading,Column,Btn } from "../Style/style-components";
+import {
+  SectionContainer,
+  ProductHeading,
+  Column,
+  Btn,
+} from "../Style/style-components";
 import { Grid, Button } from "@mui/material";
 import { Link } from "react-router-dom";
-import { useDispatch,useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { cartActions } from "../Store/cart-slice";
 function ProductItems(props) {
-
-
-  const { title, price ,id,src,width,button } = props;
+  const { title, price, id, src, width, button } = props;
 
   const dispatch = useDispatch();
 
-const addItemToCartHandler = () => {
-  dispatch(
-    cartActions.addItemToCart({
-      id,
-      title,
-      price,
-    })
-  );
-};
-
+  const addItemToCartHandler = () => {
+    dispatch(
+      cartActions.addItemToCart({
+        id,
+        title,
+        price,
+      })
+    );
+  };
 
   return (
     <>
       <SectionContainer>
-          {/* <Grid item xs={3} > */}
-            <img src={src} width={width}/>
-            <Column>
-              <ProductHeading>{title}</ProductHeading>
-              <span className="price">${price}</span>
-              
-              <Button onClick={addItemToCartHandler} variant="contained" >Add to Cart</Button>
-            </Column>  
-          {/* </Grid> */}
+        <Link to="/shop:id">
+          <img src={src} width={width} />
+          <Column>
+            <ProductHeading>{title}</ProductHeading>
+            <span className="price">${price}</span>
+            <Button onClick={addItemToCartHandler} variant="contained" >Add to Cart</Button>
+          </Column>
+        </Link>
       </SectionContainer>
     </>
   );
