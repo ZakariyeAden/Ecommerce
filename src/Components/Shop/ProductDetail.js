9090368281831344c80119a78f4999a09c4f1046
ZakariyeAden <zakariye5417@gmail>
2022-10-29 14:25:29 -0500

    starting on the product detetail page

---
+++ src/Components/Shop/ProductDetail.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import products from './index';
+import { Grid } from "@mui/material";
+function ProductDetail() {
+  return (
+    <div>
+    <p>Product page</p>
+
+    </div>
+  )
+}
+
+export default ProductDetail
