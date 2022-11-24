352f2da04c9cec2bd8ef0945bd20e3973f7cc9d0
ZakariyeAden <zakariye5417@gmail.com>
2022-11-24 16:28:20 -0600

    making the code look clean and fixing the redux issue

---
+++ src/Components/Redux/Store/index.js
@@ -0,0 +1,11 @@
+
+
+
+import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "../Reducers/rootReducer";
+
+const store = createStore(rootReducer, composeWithDevTools());
+
+export default store;
+
