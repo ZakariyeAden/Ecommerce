e9fa795814f538a6c365d1975bb4a1b7b0a3793e
ZakariyeAden <zakariye5417@gmail.com>
2022-11-24 16:20:23 -0600

    _

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './Components/App';
 import './Components/Style/index.css'
 import { Provider } from 'react-redux';
-import store from './Components/Store';
+import store from './Components/Redux/Store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
