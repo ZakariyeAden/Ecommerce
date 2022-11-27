8058f1dce692f7cc814e15d4a81a9ecc843cb527
ZakariyeAden <zakariye5417@gmail.com>
2022-11-27 09:41:05 -0600

    styling

--- src/Components/Style/style-components/index.js
+++ src/Components/Style/style-components/index.js
@@ -11,26 +11,26 @@ export const Container = styled.section`
   max-width: 1440px;
 `;
 export const Button = styled.button`
-  display:inline-block;
-  cursor:pointer;
-  font-size:1rem;
-  border:none;
-  background-color:#3a3a3a;
+  display: inline-block;
+  cursor: pointer;
+  font-size: 1rem;
+  border: none;
+  background-color: #3a3a3a;
   padding: 1rem 2rem;
-  color:#fff
+  color: #fff;
 `;
 export const SectionContainer = styled.div`
-  padding:7rem 4rem;
+  padding: 7rem 4rem;
 `;
 export const DetailContainer = styled.div`
-  padding:7rem 8rem;
-  display:flex;
-  justify-content:center;
-  align-items:center;
+  padding: 7rem 8rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const Image2 = styled.img`
-  width:98%
+  width: 98%;
 `;
 
 // Shop Section
@@ -40,7 +40,7 @@ export const ShopHeading = styled.h2`
   color: #fff;
   font-style: bold;
   font-weight: 700;
-  text-align:center;
+  text-align: center;
 `;
 // Product Section
 
@@ -51,47 +51,48 @@ export const ProductHeading = styled.h4`
 `;
 // Reuseable
 export const SecondaryHeading = styled.h3`
-  font-size:2rem;
-  letter-spacing:0.25rem;
-  color:#3a3a3a;
-  text-align:center;
+  font-size: 2rem;
+  letter-spacing: 0.25rem;
+  color: #3a3a3a;
+  text-align: center;
 `;
 export const Image = styled.img`
-  width:98%
+  width: 98%;
 `;
 export const Description = styled.p`
-  text-align:center;
-  font-size:1rem;
-  line-height:1.5;
-  font-weight:100;
+  text-align: center;
+  font-size: 1rem;
+  line-height: 1.5;
+  font-weight: 100;
 `;
 export const FooterHeading = styled.h5`
-  font-size:1.3rem;
-  font-weight:400;
-  font-style:normal;
+  font-size: 1.3rem;
+  font-weight: 400;
+  font-style: normal;
 `;
 export const Column = styled.div`
-  display:flex;
-  flex-direction:column;
+  display: flex;
+  flex-direction: column;
 `;
 export const ColumnGap = styled.div`
-  display:flex;
-  flex-direction:column;
-  gap:1rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
 `;
 export const Btn = styled.button`
-  display:inline-block;
-  cursor:pointer;
-  font-size:1rem;
-  border:none;
-  background-color:#3a3a3a;
+  display: inline-block;
+  cursor: pointer;
+  font-size: 1rem;
+  border: none;
+  background-color: #3a3a3a;
   padding: 1rem 4rem;
-  color:#fff;
+  color: #fff;
 `;
 export const DetailImg = styled.img`
-  width:250px;
+  width: 250px;
 `;
 // Cart Section
 export const CartImage = styled.img`
-  width:40px;
+  width: 40px;
 `;
+
