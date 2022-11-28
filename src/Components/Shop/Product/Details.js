ea2126424011f2908c8f017482386c1ed5263584
ZakariyeAden <zakariye5417@gmail.com>
2022-11-28 16:30:55 -0600

    starting on the form

--- src/Components/Shop/Product/Details.js
+++ src/Components/Shop/Product/Details.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React,{ useState } from "react";
 
 import { Link } from "react-router-dom";
-import { Grid, Button,InputLabel,Select,FormControl,MenuItem } from "@mui/material";
+import {
+  Grid,
+  Button,
+  InputLabel,
+  Select,
+  FormControl,
+  MenuItem,
+} from "@mui/material";
 import swal from "sweetalert";
-import { connect,useSelector } from "react-redux";
+import { connect, useSelector,useDispatch } from "react-redux";
 import { addToCart } from "../../Redux/Actions/cart-action";
 import {
   DetailContainer,
@@ -11,26 +18,35 @@ import {
   Btn,
   DetailImg,
 } from "../../Style/style-components";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 const Details = ({ current, addToCart }) => {
-// Error in Cart
-  const cart = useSelector((state) => state.cart);
+
+  const dispatch = useDispatch();
+  const [size, setSize] = useState("");
+
+
+
+
+
+  // ***** Error in Cart ****  //
+  // const cart = useSelector(state => state.cart);
   // const index = cart.find((item) => item.id === current.id);
-  // const index = cart.find((item) => item.id === current.id);
-  console.log(cart);
   
-const Modal = () => {
-  swal({
-    title: "Added to cart!",
-    icon: "success",
-    className: "alert_box",
-    title:current.title,
-    text:"Added to Cart",
-    icon: current.image,
-    imageWidth: 400,
-    imageHeight: 200,
-    timer: 1500
-  });
-};
+  // console.log(cart);
+
+  const Modal = () => {
+    swal({
+      title: "Added to cart!",
+      icon: "success",
+      className: "alert_box",
+      title: current.title,
+      text: "Added to Cart",
+      icon: current.image,
+      imageWidth: 400,
+      imageHeight: 200,
+      timer: 1500,
+    });
+  };
 
   return (
     <DetailContainer>
@@ -40,9 +56,23 @@ const Modal = () => {
         </Grid>
         <Grid md={6} sm={4}>
           <ColumnGap>
+            <FormControl fullWidth>
             <h3 className="detail-heading">{current.title}</h3>
             <p>{current.description}</p>
             <span>${current.price}</span>
+              <InputLabel id="select-label">Size</InputLabel>
+              <Select
+                labelId="select-label"
+                id="select"
+                value="Size"
+                label="Size"
+                
+              >
+                <MenuItem value="S">Small</MenuItem>
+                <MenuItem value="M">Medium</MenuItem>
+                <MenuItem value="L">Large</MenuItem>
+              </Select>
+            </FormControl>
             {/* {index > -1 ? (
               <Btn onClick={() => {addToCart(current.id); Modal()} }  variant="outlined">
               Add to Cart
@@ -53,7 +83,13 @@ const Modal = () => {
               <Basket/>
             </Btn>
             )} */}
-            <Btn onClick={() => {addToCart(current.id); Modal()} }  variant="outlined">
+            <Btn
+              onClick={() => {
+                addToCart(current.id);
+                Modal();
+              }}
+              variant="outlined"
+            >
               Add to Cart
             </Btn>
           </ColumnGap>
@@ -70,7 +106,7 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    addToCart: id => dispatch(addToCart(id) ),
+    addToCart: id => dispatch(addToCart(id)),
   };
 };
 
