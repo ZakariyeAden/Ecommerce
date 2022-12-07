0db34ff5db6a73fd95524e4800c1a1235405807f
ZakariyeAden <zakariye5417@gmail.com>
2022-12-07 17:36:50 -0600

    updating the form

--- src/Components/Pages/Contact.js
+++ src/Components/Pages/Contact.js
@@ -1,19 +1,109 @@
-import React from "react";
+import React,{ useState} from "react";
 import {
   Container,
   ShopHeading,
   ContactHeading,
   FormSection,
 } from "../Style/style-components";
-import { TextField } from "@mui/material";
 
-import { useForm } from 'react-hook-form';
+import { useForm } from "react-hook-form";
 
 function Contact() {
-  
-    const { register, handleSubmit, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);
-    console.log(errors);
+  const [submitted, setSubmitted] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  // Submit  //
+  const onSubmit = data => {
+    setSubmitted(true);
+    reset();
+    console.log(data);
+  };
+  console.log(errors);
+
+  const showThankYou = (
+    <div className="main-message">
+      <h4>Your Email has Been Sent!</h4>
+      <p>We will get in contact with you as swiftly as possible.</p>
+    </div>
+  );
+
+const showForm = (
+  <div className="form">
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="form-control">
+        <input
+          name="name"
+          type="text"
+          className="form-input"
+          
+          placeholder="Name"
+          {...register("Name", { required: true, maxLength: 80 })}
+        />
+        <span class="highlight"></span>
+        <span class="bar"></span>
+        <label htmlFor="name">Name:</label>
+        {errors.Name && <p className="error">Please check the First Name</p>}
+      </div>
+      <div className="form-control">
+        <input
+          name="mobile"
+          type="text"
+          maxLength={10}
+          className="form-input"
+          placeholder="Mobile"
+          {...register("mobile", {
+            required: true,
+            pattern: 1234567890,
+          })}
+        />
+        <span class="highlight"></span>
+        <span class="bar"></span>
+        <label htmlFor="mobile">Phone:</label>
+        {errors.mobile && <p className="error">Please check your Number</p>}
+      </div>
+      <div className="form-control">
+        <input
+          name="email"
+          type="email"
+          className="form-input"
+          placeholder="Email"
+          {...register("email", {
+            required: true,
+            pattern:
+              /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+          })}
+        />
+        <span class="highlight"></span>
+        <span class="bar"></span>
+        <label htmlFor="Email">Email:</label>
+        {errors.email && <p className="error">Please check your email</p>}
+      </div>
+      <div className="form-control">
+        <textarea
+          name="message"
+          type="text"
+          className="form-input"
+          placeholder="Message"
+          {...register("Message", { required: true, maxLength: 80 })}
+        />
+        <span class="highlight"></span>
+        <span class="bar"></span>
+        <label htmlFor="message">Text:</label>
+        {errors.Message && <p className="error">Please check your message</p>}
+      </div>
+      <div>
+        <button type="submit" className="submit-btn">
+          Submit
+        </button>
+      </div>
+    </form>
+  </div>
+);
   return (
     <Container>
       <div className="bg-img contact-bg">
@@ -23,80 +113,7 @@ function Contact() {
       </div>
       <FormSection>
         <ContactHeading>Contact Us, We will reach out to You!</ContactHeading>
-        <div className="form">
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="form-control">
-              <label htmlFor="name" className="label">
-                Name:
-              </label>
-              <input
-                name="name"
-                type="text"
-                className="form-input"
-                placeholder="Name"
-                
-          {...register("Name", {required: true, maxLength: 80})}
-              />
-          {errors.Name && <p className="error">Please check the First Name</p>}
-            </div>
-            <div className="form-control">
-              <label htmlFor="mobile" className="label">
-                Phone:
-              </label>
-              <input
-                name="mobile"
-                type="text"
-                maxLength={10}
-                className="form-input"
-                placeholder="Mobile"
-               
-                {...register("mobile", {
-              required: true,
-              pattern:
-               1234567890,
-            })}
-              />
-               {errors.mobile && <p className="error">Please check your Number</p>}
-            </div>
-            <div className="form-control">
-              <label htmlFor="Email" className="label">
-                Email:
-              </label>
-              <input
-                name="email"
-                type="email"
-                className="form-input"
-                placeholder="Email"
-                
-                {...register("email", {
-              required: true,
-              pattern:
-                /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-            })}
-              />
-               {errors.email && <p className="error">Please check your email</p>}
-            </div>
-            <div className="form-control">
-              <label htmlFor="message" className="label">
-                Text:
-              </label>
-              <textarea
-                name="message"
-                type="text"
-                className="form-input"
-                placeholder="Message"
-               
-                {...register("Message", {required: true, maxLength: 80})}
-              />
-              {errors.Message && <p className="error">Please check your message</p>}
-            </div>
-            <div>
-              <button type="submit" className="btn">
-                Submit
-              </button>
-            </div>
-          </form>
-        </div>
+        <div>{submitted ? showThankYou : showForm}</div>
       </FormSection>
     </Container>
   );
