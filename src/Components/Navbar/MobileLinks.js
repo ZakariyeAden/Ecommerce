13a1e4f964d1c990ec1b449f1e9d32fe872dc338
ZakariyeAden <zakariye5417@gmail.com>
2022-12-07 17:36:27 -0600

    _

--- src/Components/Navbar/MobileLinks.js
+++ src/Components/Navbar/MobileLinks.js
@@ -4,13 +4,13 @@ function MobileLinks() {
   return (
     <div className="mobile">
       <ul className="menu-links">
-        <Link to="/" className="link">
+        <Link to="/" className="link link-">
           Home
         </Link>
-        <Link to="/shop" className="link">
+        <Link to="/shop" className="link link-">
           Shop
         </Link>
-        <Link to="/contact" className="link">
+        <Link to="/contact" className="link link-">
           Contact
         </Link>
       </ul>
--- src/Components/Navbar/NavLinks.js
+++ src/Components/Navbar/NavLinks.js
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 function NavLinks() {
   return (
     <nav className="navbar "> 
-      <Link to="/" className="link">
+      <Link to="/" className="link link-">
         Home
       </Link>
-      <Link to="/shop" className="link">
+      <Link to="/shop" className="link link-">
         Shop
       </Link>
-      <Link to="/contact" className="link">
+      <Link to="/contact" className="link link-">
         Contact
       </Link>
     </nav>
